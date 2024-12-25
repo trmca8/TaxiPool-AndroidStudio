974597f79578ceb61ed644cbef45ad49a23e1127
Vukasin Trmčić <trmcic225@gmail.com>
2024-12-25 16:34:20 +0100

    update

--- app/src/main/java/com/example/taxipoll/adapter/KontaktAdapter.kt
+++ app/src/main/java/com/example/taxipoll/adapter/KontaktAdapter.kt
@@ -7,21 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taxipoll.R
 
-class KontaktAdapter(private val kontaktList: List<String>) : RecyclerView.Adapter<KontaktAdapter.KontaktViewHolder>() {
+class KontaktAdapter{
 
-    inner class KontaktViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val kontaktTextView: TextView = itemView.findViewById(R.id.tvNaslovVoznje)
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KontaktViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card_kontakt, parent, false)
-        return KontaktViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: KontaktViewHolder, position: Int) {
-        holder.kontaktTextView.text = kontaktList[position]
-    }
-
-    override fun getItemCount(): Int = kontaktList.size
 }
--- app/src/main/java/com/example/taxipoll/adapter/VoznjeAdapter.kt
+++ app/src/main/java/com/example/taxipoll/adapter/VoznjeAdapter.kt
@@ -7,21 +7,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.taxipoll.R
 
-class VoznjeAdapter(private val voznjeList: List<String>) : RecyclerView.Adapter<VoznjeAdapter.VoznjaViewHolder>() {
+class VoznjeAdapter{
 
-    inner class VoznjaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val voznjaTextView: TextView = itemView.findViewById(R.id.CardVoznjaTXT)
-    }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VoznjaViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.card_voznja, parent, false)
-        return VoznjaViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: VoznjaViewHolder, position: Int) {
-        holder.voznjaTextView.text = voznjeList[position]
-    }
-
-    override fun getItemCount(): Int = voznjeList.size
 }
